5e6f8a294ca20ac3f2b03d58eeb11c0bb3a807cd
michielstock <michielfmstock@gmail.com>
2020-11-06 18:52:24 +0100

    :tada: adding working gradient

---
+++ src/dynamictimewarping.jl
@@ -0,0 +1,70 @@
+#=
+Created on Friday 06 November 2020
+Last update: -
+
+@author: Michiel Stock
+michielfmstock@gmail.com
+
+Implementation of dynamic time warping and its gradients
+according to Mench and Blondel.
+=#
+
+function DPW!(D, θ)
+    n, m = size(θ)
+    @assert size(D) == (n+1, m+1) "The dimensions of the DP matrix and θ do not agree"
+	D[:,1] .= Inf
+	D[1,:] .= Inf
+	D[1,1] = 0.0
+	for i in 1:n, j in 1:m
+	 	D[i+1,j+1] = min(D[i+1,j], D[i,j], D[i,j+1]) + θ[i,j]
+	end
+    return last(D)
+end
+
+DPW(θ) = DPW!(zeros(eltype(θ), size(θ,1)+1, size(θ,2)+1), θ)
+
+
+function ∂DPW!(D, θ, E, Q)
+    n, m = size(θ)
+    fill!(Q, zero(eltype(Q)))
+	Q[end, end, 2] = 1
+	E[:,end] .= 0
+	E[end,:] .= 0
+	E[end,end] = 1
+	D[:,1] .= Inf
+	D[1,:] .= Inf
+    D[1,1] = 0.0
+    y = zeros(eltype(D), 3)
+	for i in 1:n, j in 1:m
+		y .= D[i+1,j], D[i,j], D[i,j+1]
+        D[i+1,j+1] = minimum(y) + θ[i,j]
+        Q[i+1,j+1,:] .= 0.01
+		Q[i+1,j+1,argmin(y)] = 0.98
+	end
+	for j in m:-1:1, i in n:-1:1
+        E[i+1,j+1] = Q[i+1,j+2,1] * E[i+1,j+2] + Q[i+2,j+2,2] * E[i+2,j+2] + Q[i+2,j+1,3] * E[i+2,j+1]
+	end
+	return D, E
+end
+
+
+
+function ∂DPW(θ::Matrix{T} where {T})
+    n, m = size(θ)
+    Q = zeros(n+2, m+2, 3)
+    E = zeros(n+2, m+2)
+    D = zeros(n+1, m+1)
+    return ∂DPW!(D, θ, E, Q)
+end
+
+s = sin.(0:1:20pi)
+t = cos.(1:1:10pi)
+
+θ = (s .- t').^2
+
+D, E = ∂DPW(θ)
+
+plot(heatmap(θ), heatmap(D[2:end,2:end]), heatmap(E))
+
+
+
